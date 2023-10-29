3e672f86eab9e34fb21447b931da4538b82016b2
Sheff2000 <sheffpb@gmail.com>
2023-10-29 13:07:53 +0200

    Init main App.vue

---
+++ hm07-app/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+    <div class="row">
+        <div class="col">
+            Now - ShipList
+        </div>
+        <div class="col">
+            Count - <span class="badge bg-secondary">10</span>
+        </div>
+        <div class="col">
+            Page - 1 (5)
+        </div>
+    </div>
+</template>
